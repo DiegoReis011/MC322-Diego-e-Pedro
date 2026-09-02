b58b75a4752381fa37cb2e6ea9dc3c050cb760a4
Pedro <contato@pedrohmagalhaes.com.br>
2026-09-02 19:31:10 -0300

    adiciona liga e desliga da esteira

--- src/Esteira.java
+++ src/Esteira.java
@@ -8,4 +8,24 @@ public class Esteira {
         this.item = null;
         this.emMovimento = false;
     }
+
+    public void ligar() {
+        emMovimento = true;
+    }
+
+    public void desligar() {
+        emMovimento = false;
+    }
+
+    public boolean estaEmMovimento() {
+        return emMovimento;
+    }
+
+    public boolean verificarCapacidade(double peso) {
+        return peso > 0 && peso <= capacidadeMaxima;
+    }
+
+    public double getCapacidadeMaxima() {
+        return capacidadeMaxima;
+    }
 }

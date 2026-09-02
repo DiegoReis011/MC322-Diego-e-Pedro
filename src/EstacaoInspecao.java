1cae904616bf8a6b649acf9a5caec100817da2b7
Pedro <contato@pedrohmagalhaes.com.br>
2026-09-02 19:31:44 -0300

    cria a estacao de inspecao

---
+++ src/EstacaoInspecao.java
@@ -0,0 +1,9 @@
+public class EstacaoInspecao {
+    private boolean ativa;
+    private int produtosInspecionados;
+
+    public EstacaoInspecao() {
+        this.ativa = false;
+        this.produtosInspecionados = 0;
+    }
+}

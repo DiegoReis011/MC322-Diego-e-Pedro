f7cafc864d533ac62774d4ebcb79587a2792ad88
Pedro <contato@pedrohmagalhaes.com.br>
2026-09-02 19:54:09 -0300

    monta a fabrica no main

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        MateriaPrima massaChocolate = new MateriaPrima("MP001", "Massa de chocolate", 5000, "g", 200);
-        MateriaPrima massaAveia = new MateriaPrima("MP002", "Massa de aveia", 5000, "g", 200);
-        MateriaPrima massaCoco = new MateriaPrima("MP003", "Massa de coco", 5000, "g", 200);
+        Scanner teclado = new Scanner(System.in);
+        Painel painel = new Painel(teclado);
 
-        Produto chocolate = new Produto("B001", "Bolacha de chocolate", 80, massaChocolate);
-        Produto aveia = new Produto("B002", "Bolacha de aveia", 70, massaAveia);
-        Produto coco = new Produto("B003", "Bolacha de coco", 65, massaCoco);
+        MateriaPrima massaChocolate = new MateriaPrima("MP001", "massa de chocolate", 5000, "g", 200);
+        MateriaPrima massaAveia = new MateriaPrima("MP002", "massa de aveia", 5000, "g", 200);
+        MateriaPrima massaCoco = new MateriaPrima("MP003", "massa de coco", 5000, "g", 200);
+        MateriaPrima[] massas = { massaChocolate, massaAveia, massaCoco };
 
-        mostrarProduto(chocolate);
-        mostrarProduto(aveia);
-        mostrarProduto(coco);
+        // Os dois vetores andam juntos: bolachas[i] e feita de massas[i].
+        Produto[] bolachas = {
+            new Produto("B001", "Bolacha de chocolate", 80, massaChocolate),
+            new Produto("B002", "Bolacha de aveia", 70, massaAveia),
+            new Produto("B003", "Bolacha de coco", 65, massaCoco)
+        };
 
-        chocolate.processar();
-        System.out.println("\nApós processamento:");
-        mostrarProduto(chocolate);
-    }
+        Maquina estampadeira = new Maquina("Estampadeira", 500);
+        Esteira esteira = new Esteira(600);
+        EstacaoInspecao inspecao = new EstacaoInspecao();
 
-    private static void mostrarProduto(Produto produto) {
-        System.out.println(produto.getId() + " - " + produto.getNome()
-                + " | Massa: " + produto.getMateriaPrima().getNome()
-                + " | Demanda: " + produto.getDemandaMateriaPrima() + " g"
-                + " | Status: " + produto.getStatus());
+        painel.exibirIntroducao();
+        painel.exibirEstoque(massas);
+        painel.exibirCatalogo(bolachas);
+
+        teclado.close();
     }
 }

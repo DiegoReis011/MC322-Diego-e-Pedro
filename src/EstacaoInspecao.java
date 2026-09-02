c8cee77ca8c9e56339ab3ba35de898b028f6ae10
Pedro <contato@pedrohmagalhaes.com.br>
2026-09-02 19:31:44 -0300

    adiciona ativa e desativa da inspecao

--- src/EstacaoInspecao.java
+++ src/EstacaoInspecao.java
@@ -6,4 +6,20 @@ public class EstacaoInspecao {
         this.ativa = false;
         this.produtosInspecionados = 0;
     }
+
+    public void ativar() {
+        ativa = true;
+    }
+
+    public void desativar() {
+        ativa = false;
+    }
+
+    public boolean estaAtiva() {
+        return ativa;
+    }
+
+    public int getTotalInspecionados() {
+        return produtosInspecionados;
+    }
 }

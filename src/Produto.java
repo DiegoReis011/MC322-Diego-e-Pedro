2daf0bfcc884b154be2b1149299fe1d405921734
DiegoReis011 <diegoreis0906@gmail.com>
2026-09-14 15:17:35 -0300

    atualizacao do produto

--- src/Produto.java
+++ src/Produto.java
@@ -1,50 +1,61 @@
-public class Produto {
+public abstract class Produto {
     private String id;
     private String nome;
     private String status;
-    private double quantidadeMateriaPrimaNecessaria;
-    private MateriaPrima materiaPrima;
+    private double quantidadeMateriaPrimaPorUnidade;
+    private double qualidade;
+    private double probabilidadeFalhaAcumulada;
+    private static int totalProdutosFabricados = 0;
 
-    public Produto(String id, String nome, double quantidadeMateriaPrimaNecessaria, MateriaPrima materiaPrima) {
-        this.id = id;
-        this.nome = nome;
-        this.quantidadeMateriaPrimaNecessaria = quantidadeMateriaPrimaNecessaria;
-        this.materiaPrima = materiaPrima;
-        this.status = "Aguardando processamento";
-    }
-
-    public void processar() {
-        status = "Processado";
-    }
-
-    public void aprovar() {
-        status = "Inspecionado";
-    }
-
-    public void definirDemandaMateriaPrima(double novaDemanda) {
-        if (novaDemanda > 0) {
-            quantidadeMateriaPrimaNecessaria = novaDemanda;
-        }
-    }
-
-    public double getDemandaMateriaPrima() {
-        return quantidadeMateriaPrimaNecessaria;
-    }
+    public Produto(String id, String nome, double quantidadeMateriaPrimaPorUnidade, double qualidade) {
+    this.id = id;
+    this.nome = nome;
+    this.quantidadeMateriaPrimaPorUnidade = quantidadeMateriaPrimaPorUnidade;
+    this.qualidade = qualidade;
+    this.status = "Aguardando processamento";
+    this.probabilidadeFalhaAcumulada = 0.0;
+    totalProdutosFabricados++;
+}
+    public abstract void processar();
+    public abstract double calcularTempoProducao();
+    public abstract String getTipo();
 
     public String getId() {
-        return id;
+    return id;
     }
-
+    
     public String getNome() {
         return nome;
     }
-
+    
     public String getStatus() {
         return status;
     }
-
-    // Guardar a massa aqui e o que deixa a gente saber de onde veio cada bolacha.
-    public MateriaPrima getMateriaPrima() {
-        return materiaPrima;
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    
+    public double getQuantidadeMateriaPrimaPorUnidade() {
+        return quantidadeMateriaPrimaPorUnidade;
+    }
+    
+    public double getQualidade() {
+        return qualidade;
+    }
+    
+    public void aumentarProbabilidadeFalha(double incremento) {
+        probabilidadeFalhaAcumulada += incremento;
+        if (probabilidadeFalhaAcumulada > 1.0) {
+            probabilidadeFalhaAcumulada = 1.0;
+        }
+    }
+    
+    public double getProbabilidadeFalhaAcumulada() {
+        return probabilidadeFalhaAcumulada;
+    }
+    
+    public static int getTotalProdutosFabricados() {
+        return totalProdutosFabricados;
     }
 }
